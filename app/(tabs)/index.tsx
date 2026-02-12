b20ce8fff99a061e352d8001d6d21740af3b5342
Mati Melkamu <matimelkamu15@gmail.com>
2026-02-12 12:24:24 +0300

    add convex a backend as a service

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
+import { api } from "@/convex/_generated/api";
 import { useTheme } from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { isDarkMode, toggleDarkMode, colors } = useTheme();
-  console.log(isDarkMode);
+
+  const todos = useQuery(api.todo.getTodos);
+
+  const addTodo = useMutation(api.todo.addTodo);
+  if (todos) console.log(todos);
 
   return (
     <View style={styles.container}>
       <Text style={styles.textSetting}>Todo333</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "walk the dog 55555" })}>
+        <Text>Add To do</Text>
       </TouchableOpacity>
     </View>
   );
